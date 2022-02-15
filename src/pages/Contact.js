6a020904da57357de573e18e93fd32c414dda96c
Cynthia Johnsen <vaj.cynthia@gmail.com>
2022-02-14 19:24:46 -0600

    3rd Commit

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -7,6 +7,22 @@ const Contact = () => {
         <div style={fonts}>
             <Link to="/"><img src={logo} width="100px"/></Link>
             <h1>Contact</h1>
+
+            <div>
+                <form>
+                    <label htmlFor='firstName'/>
+                    <input type='text' name='firstName' placeholder='First Name' value=''/> <br/>
+
+                    <label htmlFor='lastName'/>
+                    <input type='text' name='lastName' placeholder='Last Name' value=''/> <br/>
+
+                    <label htmlFor='email'/>
+                    <input type='email' name='lastName' placeholder='Email' value=''/> <br/>
+
+
+                    <input type='submit'/>
+                </form>
+            </div>
         </div>
     )
 }
@@ -14,9 +30,11 @@ const Contact = () => {
 const fonts={
     fontFamily: 'Architects Daughter',
     color: '#F56A34',
-    fontSize: '20pt'
+    fontSize: '20pt',
 
 }
+
+
 
 
 
